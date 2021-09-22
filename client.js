efc1136250533b8cd37ba77cf1ca1a338179112c
Eileen Xue <xue.eileen@gmail.com>
2021-09-22 18:21:31 +0000

    Setting up stdin for handling user inputs

--- client.js
+++ client.js
@@ -15,12 +15,13 @@ const connect = function() {
     console.log('Successfully connected to game server!');
     // Send my name
     conn.write("Name: EX❤️");
-    setInterval(() => {
-      conn.write("Move: up");
-    }, 100); 
-    setTimeout(() => {
-      conn.write("Move: left");
-    }, 2000); 
+    // Move Commands
+    // setInterval(() => {
+    //   conn.write("Move: up");
+    // }, 100); 
+    // setTimeout(() => {
+    //   conn.write("Move: left");
+    // }, 2000); 
   });
 
   // Display incoming data
--- play.js
+++ play.js
@@ -1,4 +1,27 @@
 const connect = require('./client');
+const stdin = process.stdin;
 
 console.log("Connecting ...");
 connect();
+
+// setup interface to handle user input from stdin
+const setupInput = function () {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+
+  // Event listener for stdin
+  stdin.on("data", handleUserInput);
+
+  return stdin;
+  
+};
+
+const handleUserInput = function () {
+  // your code here
+  if (key === '\u0003') {
+    process.exit();
+  }
+};
+
